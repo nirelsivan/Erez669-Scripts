03470d11e0c84379b415a6e9828fb564393be454
erez669 <57612608+erez669@users.noreply.github.com>
2021-04-24 21:15:28 +0300

    Add files via upload
    Script to Check Reboot Reason

---
+++ Computers-Why Rebooted.ps1
@@ -0,0 +1,30 @@
+# written by DHRUB BHARALI (https://powershellguru.com/)
+
+Function Get-ComInfo {
+param(
+## Computers
+$computers
+)
+ 
+"#"*160
+"Server Reboot Report"
+"Generated $(get-date)"
+"Generated from $(gc env:computername)"
+"#"*160
+ 
+Get-WinEvent -ComputerName $computers -FilterHashtable @{logname='System'; id=1074}  |
+ForEach-Object {
+$rv = New-Object PSObject | Select-Object Date, User, Action, Process, Reason, ReasonCode, Comment
+$rv.Date = $_.TimeCreated
+$rv.User = $_.Properties[6].Value
+$rv.Process = $_.Properties[0].Value
+$rv.Action = $_.Properties[4].Value
+$rv.Reason = $_.Properties[2].Value
+$rv.ReasonCode = $_.Properties[3].Value
+$rv.Comment = $_.Properties[5].Value
+$rv
+} | Select-Object Date, Action, Reason, User
+}
+
+############ Provide path in get content ##############
+Get-Content "\\10.251.10.251\supergsrv\HdMatte\PowerShell\Computers.txt" | ForEach-Object { Get-ComInfo -computers $_}
