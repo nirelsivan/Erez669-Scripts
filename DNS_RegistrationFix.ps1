3acf064d7d1d1d35383719b7ba24664a479f92a2
erez669 <57612608+erez669@users.noreply.github.com>
2021-05-26 16:43:31 +0300

    Add DNS Multiple Suffix

--- DNS_RegistrationFix.ps1
+++ DNS_RegistrationFix.ps1
@@ -1,9 +1,8 @@
 ﻿$networkConfig = Get-WmiObject Win32_NetworkAdapterConfiguration -filter "ipenabled = 'true'"
 $class = [wmiclass]'Win32_NetworkAdapterConfiguration'
-$class.SetDNSSuffixSearchOrder(@('corp.supersol.co.il','posprod.supersol.co.il'))
+$class.SetDNSSuffixSearchOrder(@('suffix1','suffix2'))
 $networkConfig.SetDynamicDNSRegistration($true,$true)
 wmic /interactive:off nicconfig where TcpipNetbiosOptions=0 call SetTcpipNetbios 2
 wmic /interactive:off nicconfig where TcpipNetbiosOptions=2 call SetTcpipNetbios 1
 ipconfig /flushdns
 ipconfig /registerdns
-timeout /t 4
