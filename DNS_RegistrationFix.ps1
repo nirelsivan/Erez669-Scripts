d47c663a024a195c2f81c69d77f5df31633e44fa
erez669 <57612608+erez669@users.noreply.github.com>
2021-05-26 16:42:31 +0300

    Add DNS Suffix

---
+++ DNS_RegistrationFix.ps1
@@ -0,0 +1,9 @@
+﻿$networkConfig = Get-WmiObject Win32_NetworkAdapterConfiguration -filter "ipenabled = 'true'"
+$class = [wmiclass]'Win32_NetworkAdapterConfiguration'
+$class.SetDNSSuffixSearchOrder(@('corp.supersol.co.il','posprod.supersol.co.il'))
+$networkConfig.SetDynamicDNSRegistration($true,$true)
+wmic /interactive:off nicconfig where TcpipNetbiosOptions=0 call SetTcpipNetbios 2
+wmic /interactive:off nicconfig where TcpipNetbiosOptions=2 call SetTcpipNetbios 1
+ipconfig /flushdns
+ipconfig /registerdns
+timeout /t 4
