ce022d7fb10afc00c12453f3ac1dce832873d817
erez669 <57612608+erez669@users.noreply.github.com>
2021-05-27 06:34:34 +0300

    Update DNS_RegistrationFix.ps1

--- DNS_RegistrationFix.ps1
+++ DNS_RegistrationFix.ps1
@@ -2,7 +2,7 @@
 $class = [wmiclass]'Win32_NetworkAdapterConfiguration'
 $class.SetDNSSuffixSearchOrder(@('suffix1','suffix2'))
 $networkConfig.SetDynamicDNSRegistration($true,$true)
-wmic /interactive:off nicconfig where TcpipNetbiosOptions=0 call SetTcpipNetbios 2
+wmic /interactive:off nicconfig where TcpipNetbiosOptions=0 call SetTcpipNetbios 1
 wmic /interactive:off nicconfig where TcpipNetbiosOptions=2 call SetTcpipNetbios 1
 ipconfig /flushdns
 ipconfig /registerdns
