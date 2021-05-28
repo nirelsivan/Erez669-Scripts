326e51de31fe6c3c678fe16a20e1655e9db81098
erez669 <57612608+erez669@users.noreply.github.com>
2021-05-28 10:31:44 +0300

    Update DNS_RegistrationFix.ps1

--- DNS_RegistrationFix.ps1
+++ DNS_RegistrationFix.ps1
@@ -1,7 +1,7 @@
-﻿$networkConfig = Get-WmiObject Win32_NetworkAdapterConfiguration -filter "ipenabled = 'true'"
+$networkConfig = Get-WmiObject Win32_NetworkAdapterConfiguration -filter "ipenabled = 'true'"
 $class = [wmiclass]'Win32_NetworkAdapterConfiguration'
 $class.SetDNSSuffixSearchOrder(@('suffix1','suffix2'))
-$networkConfig.SetDynamicDNSRegistration($true,$true)
+$networkConfig.SetDynamicDNSRegistration($true,$false)
 wmic /interactive:off nicconfig where TcpipNetbiosOptions=0 call SetTcpipNetbios 1
 wmic /interactive:off nicconfig where TcpipNetbiosOptions=2 call SetTcpipNetbios 1
 Clear-DnsClientCache -Verbose
