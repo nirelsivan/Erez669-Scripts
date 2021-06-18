3902341cd53c6a2af9152f480ce54d5935d59636
erez669 <57612608+erez669@users.noreply.github.com>
2021-06-18 12:13:53 +0300

    Update OpenAdminShare.ps1

--- OpenAdminShare.ps1
+++ OpenAdminShare.ps1
@@ -2,7 +2,7 @@ $Computer = read-host 'Enter RemoteMachine Name\IP Address'
 $UserName = read-host 'Enter ADM User'
 $password = read-host 'Enter ADM Password' -AsSecureString
 $Newpass = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
-$Domain = "corp.supersol.co.il"
+$Domain = "mydomain.com"
 $UNCServer = "\\$Computer\C$"
 net use /delete $UNCServer
 net use $UNCServer $Newpass /user:$Domain\$UserName
