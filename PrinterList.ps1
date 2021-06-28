eb12785b36f51e77c2b820aa7a57c40ff88b9213
erez669 <57612608+erez669@users.noreply.github.com>
2021-06-28 14:04:43 +0300

    Update PrinterList.ps1

--- PrinterList.ps1
+++ PrinterList.ps1
@@ -3,7 +3,7 @@ cls
 write-host "Get Printer List from Printer Server"
 write-host "`n" # for creating space
 $printserver = read-host "enter printer server name"
-Get-Printer -ComputerName $printserver | select name, portname > "\\10.251.10.251\Supergsrv\HdMatte\NetworkPrinterList\PrinterList-$printserver.txt"
+Get-Printer -ComputerName $printserver | select name, portname | out-file "\\10.251.10.251\Supergsrv\HdMatte\NetworkPrinterList\PrinterList-$printserver.txt"
 Invoke-Item "\\10.251.10.251\Supergsrv\HdMatte\NetworkPrinterList\PrinterList-$printserver.txt"
 }
 $while
