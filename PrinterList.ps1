e89be097c064c4056bda96074fafd6453b2e0929
erez669 <57612608+erez669@users.noreply.github.com>
2021-06-28 12:45:37 +0300

    Get Printers List from Printer Server

---
+++ PrinterList.ps1
@@ -0,0 +1,9 @@
+﻿while ($true) {
+cls
+write-host "Get Printer List from Printer Server"
+write-host "`n" # for creating space
+$printserver = read-host "enter printer server name"
+Get-Printer -ComputerName $printserver | select name, portname > "\\10.251.10.251\Supergsrv\HdMatte\NetworkPrinterList\PrinterList-$printserver.txt"
+Invoke-Item "\\10.251.10.251\Supergsrv\HdMatte\NetworkPrinterList\PrinterList-$printserver.txt"
+}
+$while
