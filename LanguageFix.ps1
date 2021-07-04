e21bb4b5c6e92f6f31a72d19b5cb325a427c11ac
erez669 <57612608+erez669@users.noreply.github.com>
2021-07-04 13:49:20 +0300

    Update LanguageFix.ps1

--- LanguageFix.ps1
+++ LanguageFix.ps1
@@ -1,8 +1,3 @@
-<#
-new-item 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\powershell' -force
-Set-Itemproperty -path HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell -name ExecutionPolicy -value ByPass -force
-new-Itemproperty -path HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell -name EnableScripts -PropertyType Dword -Value 1 -force
-#>
 Import-Module International
 $LangList = Get-WinUserLanguageList
 $MarkedLang = $LangList | where LanguageTag -eq "he"
@@ -31,5 +26,5 @@ REG ADD "HKU\DefaultUserTemplate\Keyboard Layout\Preload" /v "1" /t REG_SZ /d 00
 REG ADD "HKU\DefaultUserTemplate\Keyboard Layout\Preload" /v "2" /t REG_SZ /d 0000040d /f
 REG UNLOAD "HKU\DefaultUserTemplate"
 Write-Output "Successfully Changing the default keyboard layout"
-rundll32.exe shell32,Control_RunDLL intl.cpl
-# Remove-Item 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\powershell' -force
+& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"\\10.251.10.251\supergsrv\HdMatte\PowerShell\Lang.xml`""
+Invoke-Command {reg import "\\10.251.10.251\supergsrv\HdMatte\PowerShell\Default_Lang.reg"}
