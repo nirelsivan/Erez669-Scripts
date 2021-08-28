c34f62be72abb2030460c0c18107e03564729d6d
erez669 <57612608+erez669@users.noreply.github.com>
2021-08-28 13:15:44 +0300

    Export_ComputerList_SpecificOU
    Export_ComputerList_SpecificOU

---
+++ Export_ComputerList_SpecificOU.ps1
@@ -0,0 +1,3 @@
+﻿$OUpath = 'OU=Test,OU=Workstations,DC=corp,DC=supersol,DC=co,DC=il'
+$ExportPath = 'C:\Temp\Test.txt'
+Get-ADComputer -Filter * -SearchBase $OUpath | Select-object Name | Out-File $ExportPath
