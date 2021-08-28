291f16458a2b66a62b72b0833912b47e8bf6e6af
erez669 <57612608+erez669@users.noreply.github.com>
2021-08-28 12:42:20 +0300

    PrinterList.bat
    PrinterList.bat

---
+++ PrinterList.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ****** Get Printer List from Printer Server ****** 
+pushd "%~dp0"
+powershell.exe -Command "Start-Process powershell \"new-item 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\powershell' -force"" -Verb RunAs"
+powershell.exe -Command "Start-Process powershell \"Set-Itemproperty -path HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell -name ExecutionPolicy -value ByPass"" -Verb RunAs"
+powershell.exe -Command "Start-Process powershell \"new-Itemproperty -path HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell -name EnableScripts -PropertyType Dword -Value 1 -force"" -Verb RunAs"
+powershell.exe -Command "Start-Process 'cmd' -Verb RunAs -ArgumentList '/c "powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0PrinterList.ps1"'"
