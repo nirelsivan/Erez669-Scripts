f8cd1faab2da4d591a11fe5fbd572fd2550d60fc
erez669 <57612608+erez669@users.noreply.github.com>
2021-08-28 13:14:57 +0300

    MoveOU_FromFile
    MoveOU_FromFile

---
+++ MoveOU_MultipleOjbejcts.ps1
@@ -0,0 +1,6 @@
+﻿$computerlist = get-content -path "\\10.251.10.251\supergsrv\HdMatte\PowerShell\Computers.txt"
+foreach ($computer in $computerlist) {
+$newou = "OU=Test,OU=MarlogRishon,OU=Workstations,DC=corp,DC=supersol,DC=co,DC=il"
+Get-ADComputer $computer |Move-ADObject -TargetPath $newou -Verbose # check the current AD object OU and moving it to the requested OU
+}
+Write-Host "operation completed successfully"
