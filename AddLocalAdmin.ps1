8df53f4acfc7529e72002c527b6dd7ac256d514f
erez669 <57612608+erez669@users.noreply.github.com>
2021-09-01 21:13:20 +0300

    Update AddLocalAdmin.ps1

--- AddLocalAdmin.ps1
+++ AddLocalAdmin.ps1
@@ -20,14 +20,14 @@ write-host "`n" # for creating space
 $UserName = Read-Host -Prompt 'Enter Destination UserName'
 $Computer = Read-Host -Prompt 'Enter Hostname\IP Address'
 $AdminGroup = "Administrators"
-$Domain = "mydomain"
-$DomainCredential = Get-Credential "mydomain\***adm"
+$Domain = "corp.supersol.co.il"
+$DomainCredential = Get-Credential "supersol\***adm"
 ([ADSI]"WinNT://$Computer/$AdminGroup,group").psbase.Invoke("Add",([ADSI]"WinNT://$Domain/$UserName").path)
 write-host "`n" # for creating space
 write-output "Operation Complete, wait for logout"
-timeout /t 5
 $sessionId = ((quser /server:$Computer | Where-Object { $_ -match $UserName }) -split ' +')[2]
 $sessionId
 logoff $sessionId /server:$Computer
+timeout /t 5
 }
 $while
