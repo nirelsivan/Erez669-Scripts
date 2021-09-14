ccb4f6d9d67f6e6b0263f6fc97db595540ea2991
erez669 <57612608+erez669@users.noreply.github.com>
2021-09-14 15:02:11 +0300

    Restart Service
    Restart Service

---
+++ Restart_Service.ps1
@@ -0,0 +1,13 @@
+﻿while ($true) {
+cls
+write-host "Restart Landesk Remote Service for bypass Remote Control bug"
+write-host "`n" # for creating space
+$ComputerName = Read-Host "Enter PC Name"
+write-host "`n" # for creating space
+$Service = Get-Service -ComputerName $ComputerName | Where {$_.DisplayName -like "*landesk remote*"}
+echo $Service
+Restart-Service -InputObject $Service
+write-host "`n" # for creating space
+echo $Service
+Start-Sleep -s 05
+}
