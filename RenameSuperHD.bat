c4598e25dbe6163e446b7248a6487ab0eb95ff86
erez669 <57612608+erez669@users.noreply.github.com>
2021-10-17 12:53:51 +0300

    Update RenameSuperHD.bat

--- RenameSuperHD.bat
+++ RenameSuperHD.bat
@@ -1,5 +1,5 @@
 @echo off
-pushd "%~dp0
+pushd "%~dp0"
 powershell.exe -Command "Start-Process powershell \"new-item 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\powershell' -force"" -Verb RunAs"
 powershell.exe -Command "Start-Process powershell \"Set-Itemproperty -path HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell -name ExecutionPolicy -value ByPass"" -Verb RunAs"
 powershell.exe -Command "Start-Process powershell \"new-Itemproperty -path HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell -name EnableScripts -PropertyType Dword -Value 1 -force"" -Verb RunAs"
