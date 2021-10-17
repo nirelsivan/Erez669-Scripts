913221094ffdbdff34ea49c4631617c74e0037cd
erez669 <57612608+erez669@users.noreply.github.com>
2021-10-17 12:52:54 +0300

    Update RenameSuperHD.ps1

--- RenameSuperHD.ps1
+++ RenameSuperHD.ps1
@@ -1,4 +1,4 @@
 ﻿New-PSDrive -name "PublicDesk" -PSProvider FileSystem -Root "C:\Users\Public\desktop\"
 $dest = "C:\Users\Public\desktop\"
-Rename-Item -Path "C:\Users\Public\desktop\superhd.url" -NewName "C:\Users\Public\desktop\מרכז תמיכה שופרסל.url"
+Rename-Item -Path "C:\Users\Public\desktop\superhd.url" -NewName "C:\Users\Public\desktop\מרכז תמיכה.url"
 Remove-PSDrive -name "PublicDesk"
