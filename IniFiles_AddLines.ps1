2de5f90cab9afc591d3aacbdea60f50b4fa791d9
erez669 <57612608+erez669@users.noreply.github.com>
2021-10-31 16:07:22 +0200

    Add lines to INI Files
    Add lines to INI Files

---
+++ IniFiles_AddLines.ps1
@@ -0,0 +1,9 @@
+﻿#-------------------- Input --------------------#
+$InputFilePath = "C:\temp\config.ini";
+$OutputFilePath = "C:\temp\config.ini";
+$LineNumber=7;
+$Content="UpdateServer=true";
+#-------------------- Command ------------------#
+$Count=0; $Result = cat $InputFilePath | % {$Count++; if($Count -eq $LineNumber){ Write-Output $Content; Write-Output $_ } else { Write-Output $_ }}
+$Result > $OutputFilePath;
+#--------------------- End ---------------------#
