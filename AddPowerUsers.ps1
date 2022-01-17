5e84ab83334cc71661fbd60639419990d305c78e
erez669 <57612608+erez669@users.noreply.github.com>
2022-01-17 13:14:16 +0200

    Add Users to Power Users Group
    Add Users to Power Users Group

---
+++ AddPowerUsers.ps1
@@ -0,0 +1,33 @@
+﻿#Elevate Credentials
+param([switch]$Elevated)
+function Check-Admin {
+$currentUser = New-Object Security.Principal.WindowsPrincipal `
+$([Security.Principal.WindowsIdentity]::GetCurrent())
+$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) }
+if ((Check-Admin) -eq $false) {
+    if ($elevated){
+} else {
+    Start-Process powershell.exe -Verb RunAs -ArgumentList `
+ ('-noprofile -noexit -file "{0}" -elevated' -f ( $myinvocation.MyCommand.Definition ))
+    } exit
+}
+#Start Main Script
+
+while ($true) {
+cls
+write-output "Add User to Power Users Group"
+write-host "`n" # for creating space
+$UserName = Read-Host -Prompt 'Enter Destination UserName'
+$Computer = Read-Host -Prompt 'Enter Hostname\IP Address'
+$PowerGroup = "Power Users"
+$Domain = "corp.supersol.co.il"
+$DomainCredential = Get-Credential "supersol\***adm"
+([ADSI]"WinNT://$Computer/$PowerGroup,group").psbase.Invoke("Add",([ADSI]"WinNT://$Domain/$UserName").path)
+write-host "`n" # for creating space
+write-output "Operation Complete, wait for logout"
+$sessionId = ((quser /server:$Computer | Where-Object { $_ -match $UserName }) -split ' +')[2]
+$sessionId
+logoff $sessionId /server:$Computer
+timeout /t 5
+}
+$while
