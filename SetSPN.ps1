f035bb1f551fab26a078dcf96e5243928257fe58
erez669 <57612608+erez669@users.noreply.github.com>
2022-01-27 12:40:53 +0200

    Update SetSPN.ps1

--- SetSPN.ps1
+++ SetSPN.ps1
@@ -1,19 +1,30 @@
-﻿$spn = Read-Host 'enter snif number'
+while ($true) {
+cls
+Write-Host "Update ADSI SPN Values"
+write-host "`n" # for creating space
+$spn = Read-Host 'enter snif number'
 setspn -d MSSQLSvc/plpossrv$spn posprod\sqlsnif
 setspn -d MSSQLSvc/plpossrv$spn.posprod.supersol.co.il posprod\sqlsnif
 setspn -d MSSQLSvc/plpossrv$spn.posprod.supersol.co.il:1433 posprod\sqlsnif
-setspn -d MSSQLSvc/plpossrv$spn posprod\sql2017
+setspn -d MSSQLSvc/plpossrv$spn.posprod.supersol.co.il posprod\sql2017
+setspn -d MSSQLSvc/plpossrv$spn.posprod.supersol.co.il:1433 posprod\sql2017
 setspn -d MSSQLSvc/slpossrv$spn posprod\sqlsnif
 setspn -d MSSQLSvc/slpossrv$spn.posprod.supersol.co.il posprod\sqlsnif
 setspn -d MSSQLSvc/slpossrv$spn.posprod.supersol.co.il:1433 posprod\sqlsnif
-setspn -d MSSQLSvc/slpossrv$spn posprod\sql2017
+setspn -d MSSQLSvc/slpossrv$spn.posprod.supersol.co.il posprod\sql2017
+setspn -d MSSQLSvc/slpossrv$spn.posprod.supersol.co.il:1433 posprod\sql2017
 setspn -d MSSQLSvc/wlpossrv$spn posprod\sqlsnif
 setspn -d MSSQLSvc/wlpossrv$spn.posprod.supersol.co.il posprod\sqlsnif
 setspn -d MSSQLSvc/wlpossrv$spn.posprod.supersol.co.il:1433 posprod\sqlsnif
-setspn -d MSSQLSvc/wlpossrv$spn posprod\sql2017
+setspn -d MSSQLSvc/wlpossrv$spn.posprod.supersol.co.il posprod\sql2017
+setspn -d MSSQLSvc/wlpossrv$spn.posprod.supersol.co.il:1433 posprod\sql2017
+setspn -s MSSQLSvc/plpossrv$spn posprod\sql2017
 setspn -s MSSQLSvc/plpossrv$spn.posprod.supersol.co.il posprod\sql2017
 setspn -s MSSQLSvc/plpossrv$spn.posprod.supersol.co.il:1433 posprod\sql2017
+setspn -s MSSQLSvc/slpossrv$spn posprod\sql2017
 setspn -s MSSQLSvc/slpossrv$spn.posprod.supersol.co.il posprod\sql2017
 setspn -s MSSQLSvc/slpossrv$spn.posprod.supersol.co.il:1433 posprod\sql2017
-setspn -s MSSQLSvc/wlpossrv$spn.posprod.supersol.co.il:1433 posprod\sql2017
+setspn -s MSSQLSvc/wlpossrv$spn posprod\sql2017
 setspn -s MSSQLSvc/wlpossrv$spn.posprod.supersol.co.il posprod\sql2017
+setspn -s MSSQLSvc/Wlpossrv$spn.posprod.supersol.co.il:1433 posprod\sql2017
+}
