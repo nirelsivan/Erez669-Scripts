1e4f0b52f7817bdf065a3ccb8e37bd4a68e68505
erez669 <57612608+erez669@users.noreply.github.com>
2022-02-07 13:08:36 +0200

    Update AddDrv.cmd

--- AddDrv.cmd
+++ AddDrv.cmd
@@ -1,7 +1,5 @@
 @echo off
 Echo adding drivers to Image
-call c:\ps\cert\RunCert.bat
-Echo adding drivers to Image
 if not exist c:\drivers goto :end
 FOR /F "usebackq delims=" %%i IN (`dir c:\drivers\* /S /B`) do (
   if /I "%%~xi" == ".inf" (
@@ -10,10 +8,6 @@ FOR /F "usebackq delims=" %%i IN (`dir c:\drivers\* /S /B`) do (
     pnputil -i -a %%i >> c:\AddDrv.txt
   )
 )
-netsh advfirewall set allprofiles state off
-rem ***Add Schedule Task To Remove Welcome Screen Popup - AutoLogon***
-rem schtasks.exe /create /tn "ClearPolicy" /ru SYSTEM /Sc ONSTART /tr "C:\ps\ClearPolicy.cmd"
-
 
 :end
 exit /b 0
