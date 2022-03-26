1538f7a8bb7f7be6fb63679260c8aeeeb3a1a83a
erez669 <57612608+erez669@users.noreply.github.com>
2022-03-26 21:41:57 +0300

    run batch file on multiple servers
    run batch file on multiple servers

---
+++ Runcommand_servers.ps1
@@ -0,0 +1,21 @@
+﻿#Variables
+$servers = "D:\temp\Servers.txt"
+$computername = Get-Content $servers
+$sourcefile = "c:\ps\cb8x64.bat"
+#This section will install the software 
+foreach ($computer in $computername) 
+{
+    $destinationFolder = "\\$computer\C$\ps"
+    <#
+       It will copy $sourcefile to the $destinationfolder. If the Folder does 
+       not exist it will create it.
+    #>
+
+    if ((Test-Path -path $destinationFolder))
+    {
+          New-Item $destinationFolder -Type Directory -Force
+    }
+    Copy-Item -Path $sourcefile -Destination $destinationFolder
+    Invoke-Command -ComputerName $computer -ScriptBlock {Start-Process -FilePath 'c:\ps\cb8x64.bat' -NoNewWindow -Wait -PassThru}
+
+}
