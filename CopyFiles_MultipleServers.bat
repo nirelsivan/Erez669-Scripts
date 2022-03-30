839c1f7afd64d65d2bd758ec9a97dcb558f08b3c
erez669 <57612608+erez669@users.noreply.github.com>
2022-03-30 08:31:52 +0300

    Copy Files\Folders to Multiple Servers
    Copy Files\Folders to Multiple Servers

---
+++ CopyFiles_MultipleServers.bat
@@ -0,0 +1,7 @@
+@echo off
+pushd "%~dp0"
+powershell.exe -Command "Start-Process powershell \"Set-Itemproperty -path HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell -name ExecutionPolicy -value ByPass"" -Verb RunAs"
+powershell.exe -Command "Start-Process 'powershell' -Verb RunAs -ArgumentList '/c "powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0CopyFiles_MultipleServers.ps1"'"
+powershell.exe -Command "Start-Process powershell \"Set-Itemproperty -path HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell -name ExecutionPolicy -value RemoteSigned"" -Verb RunAs"
+popd
+exit /b 0
---
+++ CopyFiles_MultipleServers.ps1
@@ -0,0 +1,18 @@
+﻿     #Made By Erez Schwartz
+
+# This file contains the list of servers you want to copy files/folders to
+$servers = gc "D:\temp\Servers.txt"
+ 
+# This is the file/folder(s) you want to copy to the servers in the $computer variable
+$source = "C:\Fix_IIS"
+ 
+# The destination location you want the file/folder(s) to be copied to
+$destination = "d$\IvantiShare\Packages\Servers_PL_SL\Server2019\"
+ 
+foreach ($server in $servers) {
+if ((Test-Path -Path \\$server\$destination)) {
+Copy-Item $source -Destination \\$server\$destination -Recurse -Verbose -Force
+} else {
+"\\$server\$destination is not reachable or does not exist"
+}
+}
