c8d0ffa5bf1bde2877f5fff92bea10333292d3b5
erez669 <57612608+erez669@users.noreply.github.com>
2022-04-03 20:30:59 +0300

    Update Date_Modified_v2.ps1

--- Date_Modified_v2.ps1
+++ Date_Modified_v2.ps1
@@ -1,7 +1,7 @@
-$temp = Get-ChildItem c:\retalix\wingpos\Files\Images\CustomerScreen\Backgrounds\* | where {$_.LastWriteTime -ge (Get-Date).AddMonths(-12)} | select name, lastwritetime | export-csv d:\install\$env:COMPUTERNAME.csv
-$Date = Get-ChildItem c:\retalix\wingpos\Files\Images\CustomerScreen\Backgrounds\vcd_idle.png | where {$_.LastWriteTime -ge (Get-Date).AddMonths(-12)} | select lastwritetime
+$temp = Get-ChildItem c:\Backgrounds\* | where {$_.LastWriteTime -ge (Get-Date).AddMonths(-12)} | select name, lastwritetime | export-csv d:\install\$env:COMPUTERNAME.csv
+$Date = Get-ChildItem c:\Backgrounds\vcd_idle.png | where {$_.LastWriteTime -ge (Get-Date).AddMonths(-12)} | select lastwritetime
 if  ( $Date -eq "13/02/2022 15:44:15" ){
 exit }
-else {Copy-Item "D:\Install\$env:COMPUTERNAME.csv" -Destination "\\ivantiappsrv.corp.supersol.co.il\IvantiShare\Packages\POS\POSLOGO_DateModified" }
+else {Copy-Item "D:\Install\$env:COMPUTERNAME.csv" -Destination "\\myserver\LOGO_DateModified" }
 
 exit
