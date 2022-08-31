1efe76b637a01864e8ffa084696d131dd8f2842b
erez669 <57612608+erez669@users.noreply.github.com>
2022-08-31 19:32:04 +0300

    check if network card enabled or not
    check if network card enabled or not

---
+++ Network_Adapter_Status.ps1
@@ -0,0 +1,10 @@
+﻿        # written by Elik and ErezSc
+
+$ErrorActionPreference = 'SilentlyContinue'
+
+$status = Get-NetAdapter | Select-Object -ExpandProperty "status" -Unique
+if ($status -contains "Disabled") {
+Write-Output "One of the Adapters is Down" | Out-File -FilePath "c:\install\AdapterStatus.txt" -Append
+} elseif ($status -eq "Up") {
+Remove-Item "c:\install\AdapterStatus.txt"
+}
