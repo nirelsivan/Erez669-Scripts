4cb61fc2bcddc08f1494d9513cf502176876b02c
erez669 <57612608+erez669@users.noreply.github.com>
2022-09-13 15:42:48 +0300

    Update CreateADUsers_Bulk.ps1

--- CreateADUsers_Bulk.ps1
+++ CreateADUsers_Bulk.ps1
@@ -1,4 +1,4 @@
-﻿# Import active directory module for running AD cmdlets
+# Import active directory module for running AD cmdlets
 Import-Module activedirectory
   
 #Store the data from ADUsers.csv in the $ADUsers variable
@@ -7,13 +7,13 @@ $ADUsers = Import-csv C:\PowerShell\NewUsers.csv
 #Loop through each row containing user details in the CSV file 
 foreach ($User in $ADUsers)
 {
-	#Read user data from each field in each row and assign the data to a variable as below
+    #Read user data from each field in each row and assign the data to a variable as below
 		
-	$Username 	= $User.username
-	$Password 	= $User.password
-	$Firstname 	= $User.firstname
-	$Lastname 	= $User.lastname
-	$OU 		= $User.ou #This field refers to the OU the user account is to be created in
+    $Username 	= $User.username
+    $Password 	= $User.password
+    $Firstname 	= $User.firstname
+    $Lastname 	= $User.lastname
+    $OU 	= $User.ou #This field refers to the OU the user account is to be created in
     $email      = $User.email
     $streetaddress = $User.streetaddress
     $city       = $User.city
@@ -30,17 +30,17 @@ foreach ($User in $ADUsers)
 	#Check to see if the user already exists in AD
 	if (Get-ADUser -F {SamAccountName -eq $Username})
 	{
-		 #If user does exist, give a warning
-		 Write-Warning "A user account with username $Username already exist in Active Directory."
+	     #If user does exist, give a warning
+	     Write-Warning "A user account with username $Username already exist in Active Directory."
 	}
 	else
 	{
-		#User does not exist then proceed to create the new user account
+             #User does not exist then proceed to create the new user account
 		
         #Account will be created in the OU provided by the $OU variable read from the CSV file
-		New-ADUser `
+	New-ADUser `
             -SamAccountName $Username `
-            -UserPrincipalName "$Username@shufersal.co.il" `
+            -UserPrincipalName "$Username@company.co.il" `
             -Name "$Firstname $Lastname" `
             -GivenName $Firstname `
             -Surname $Lastname `
