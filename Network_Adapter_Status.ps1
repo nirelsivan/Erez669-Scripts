1b282540c04d6010fb32941eeeedc8610e8802f1
erez669 <57612608+erez669@users.noreply.github.com>
2022-09-17 21:51:08 +0300

    Update Network_Adapter_Status.ps1

--- Network_Adapter_Status.ps1
+++ Network_Adapter_Status.ps1
@@ -1,9 +1,9 @@
-      # written by Erez Schwartz
+        # written by Erez Schwartz
 
 $ErrorActionPreference = 'SilentlyContinue'
 
 $status = Get-NetAdapter | Select-Object -ExpandProperty "status" -Unique
-if ($status -contains "Disabled") {
+if ($status -contains "Disabled" -or $status -contains "Disconnected") {
 Write-Output "One of the Adapters is Down" | Out-File -FilePath "c:\install\AdapterStatus.txt" -Append
 } elseif ($status -eq "Up") {
 Remove-Item "c:\install\AdapterStatus.txt"
