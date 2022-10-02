15792437c984177aa88996ee941001407e9c8db0
erez669 <57612608+erez669@users.noreply.github.com>
2022-10-02 18:34:26 +0300

    Network Adapter Status v2
    Network Adapter Status v2

---
+++ NetworkAdapterStatus-Mon.ps1
@@ -0,0 +1,21 @@
+﻿#################################################################################
+# Network Adapter Monitoring
+# FileName: NetworkAdapter-Mon.ps1
+# Description: Monitors multiple Network adapters status
+#              Designed specially for witness Servers on snifim
+# Executing script: powershell NetworkAdapterMon.ps1
+#
+# version: 1.0
+# Author: Eli Kastika & Erez Schwartz
+#################################################################################
+$srvName = [System.Net.Dns]::GetHostName()
+$1stNum = $srvName.Substring($srvName.Length -3 ,3)[0]
+$2ndNum = $srvName.Substring($srvName.Length -3 ,3)[1]
+$3rdNum = $srvName.Substring($srvName.Length -3 ,3)[2]
+
+$Check1 = Test-Connection 10.11$1stNum.$2ndNum$3rdNum.9 -Quiet -Count 5 
+
+$check2 = Test-Connection 10.11$1stNum.$2ndNum$3rdNum.8 -Quiet -Count 5 
+
+if ($Check1) {echo "Adapter 9 Is Up"} else {echo "Adapter 9 Is Down"}
+if ($Check2) {echo "Adapter 8 Is Up"} else {echo "Adapter 8 Is Down"}
