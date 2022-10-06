e709b2fe0433904fe3b51140bf5c07c5b4f61671
erez669 <57612608+erez669@users.noreply.github.com>
2022-10-06 15:28:33 +0300

    Update Network_Adapter_Status_Monitor.ps1

--- Network_Adapter_Status_Monitor.ps1
+++ Network_Adapter_Status_Monitor.ps1
@@ -2,8 +2,8 @@
 # Network Adapter Monitoring
 # FileName: Network_Adapter_Status_Monitor.ps1
 # Description: Monitors multiple Network adapters status
-#              Designed specially for witness Servers on snifim
-# Executing script: powershell NetworkAdapterMon.ps1
+#              Designed specially for witness Servers
+# Executing script: powershell Network_Adapter_Status_Monitor.ps1
 #
 # version: 2.0
 # Author: Eli Kastika & Erez Schwartz
@@ -13,8 +13,20 @@ $1stNum = $srvName.Substring($srvName.Length -3 ,3)[0]
 $2ndNum = $srvName.Substring($srvName.Length -3 ,3)[1]
 $3rdNum = $srvName.Substring($srvName.Length -3 ,3)[2]
 
-$Check1 = Test-Connection 10.11$1stNum.$2ndNum$3rdNum.9 -Quiet -Count 5 
-$check2 = Test-Connection 10.11$1stNum.$2ndNum$3rdNum.8 -Quiet -Count 5 
+$Check1 = Test-Connection 10.11$1stNum.$2ndNum$3rdNum.9 -Quiet -Count 5
+$check2 = Test-Connection 10.11$1stNum.$2ndNum$3rdNum.8 -Quiet -Count 5
 
-if ($Check1) {echo "Adapter 9 Is Up"} else {echo "Adapter 9 Is Down"}
-if ($Check2) {echo "Adapter 8 Is Up"} else {echo "Adapter 8 Is Down"}
+echo "<metadata>MonitorTypeName=NetAdapterMon</metadata>"
+if ($Check1) {
+    echo "NetAdapterMon=NetworkAdapter-9,status=0"
+} 
+    else {
+        echo "NetAdapterMon=NetworkAdapter-9,status=1"
+    }
+
+if ($Check2) {
+    echo "NetAdapterMon=NetworkAdapter-8,status=0"
+} 
+    else {
+        echo "NetAdapterMon=NetworkAdapter-8,status=1"
+}
