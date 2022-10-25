064aac9e6f578fd04c3e883a799a6a4399760a29
erez669 <57612608+erez669@users.noreply.github.com>
2022-10-25 22:29:01 +0300

    Reboot Server Onetime
    Reboot Server Onetime

---
+++ OneTime_Reboot.ps1
@@ -0,0 +1,6 @@
+﻿$Action = New-ScheduledTaskAction -Execute 'shutdown.exe -r -t 0 -f'
+$date = (([DateTime]::Today).AddDays(1)).AddHours(2)
+$Trigger = New-ScheduledTaskTrigger -Once -at $date
+$Settings = New-ScheduledTaskSettingsSet
+$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
+Register-ScheduledTask -TaskName 'Reboot' -InputObject $Task -User system
