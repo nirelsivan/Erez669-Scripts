31a537386766d4dc044d16cb91fd6619274a80bb
erez669 <57612608+erez669@users.noreply.github.com>
2022-10-25 22:36:08 +0300

    Update OneTime_Reboot.ps1

--- OneTime_Reboot.ps1
+++ OneTime_Reboot.ps1
@@ -1,5 +1,5 @@
 ﻿$Action = New-ScheduledTaskAction -Execute 'shutdown.exe -r -t 0 -f'
-$date = (([DateTime]::Today).AddDays(1)).AddHours(2)
+$date = (([DateTime]::Today).AddDays(1)).AddHours(2) # Returns a new DateTime that adds the specified number of hours to the value of this instance.
 $Trigger = New-ScheduledTaskTrigger -Once -at $date
 $Settings = New-ScheduledTaskSettingsSet
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
