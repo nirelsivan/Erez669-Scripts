cbcf178063a5712a4e3fb7cbb0733ea214f2d9a5
erez669 <57612608+erez669@users.noreply.github.com>
2022-11-05 21:33:58 +0200

    Test connection for multiple devices
    Test connection for multiple devices and export to .txt file

---
+++ Test-Connection_MultipleServers.ps1
@@ -0,0 +1,21 @@
+﻿$complist = gc "\\10.251.10.251\supergsrv\HdMatte\PowerShell\Servers.txt"
+$Results = "c:\temp\Results.txt"
+
+$OutputMessage = @()
+
+foreach($comp in $complist){
+    
+    $pingtest = Test-Connection -ComputerName $comp -Quiet -Count 2 -ErrorAction SilentlyContinue
+
+    if($pingtest){
+         write-host $comp is online
+         $OutputMessage += ("$comp is online")
+     }
+     else{
+         write-host $comp is not reachable
+         $OutputMessage += ("$comp is not reachable")
+     }
+     
+}
+
+$OutputMessage | Out-File $Results -Verbose -Append
