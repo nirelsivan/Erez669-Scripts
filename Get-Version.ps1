1b49e1a3d50d85a5b9c7726df8309a85b0b9ee41
erez669 <57612608+erez669@users.noreply.github.com>
2022-12-08 21:41:45 +0200

    Get-FileVersion Info
    Get-FileVersion Info

---
+++ Get-Version.ps1
@@ -0,0 +1,10 @@
+﻿clear
+$servers = get-content c:\temp\PC.txt
+$file = 'c:\temp\test.csv'
+
+foreach ($server in $servers)
+{
+write-host "Files on '$server':" -foregroundcolor yellow -Verbose
+invoke-command -computername $server {(get-childitem -path "c:\psoftw\piryon2.exe" -Recurse -Verbose).VersionInfo
+} | select -Property PSComputerName , FileVersion | Export-Csv -Path $file -Append -Verbose -Force -Encoding UTF8 -NoTypeInformation
+}
