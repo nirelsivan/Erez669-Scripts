86f808945b3053273e3552d370eb8b4ae5fa2262
erez669 <57612608+erez669@users.noreply.github.com>
2022-12-13 18:28:17 +0200

    Schedule Weekly Reboot
    Schedule Weekly Reboot

---
+++ Create_Weekly_Reboot.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'SilentlyContinue'
+Get-ScheduledTask -TaskName "Reboot" -TaskPath \
+Unregister-ScheduledTask -TaskName "Reboot" -TaskPath \ -Confirm:$false
+$Action = New-ScheduledTaskAction -Execute 'shutdown.exe' -Argument '-r -t 0 -f'
+$Trigger = New-ScheduledTaskTrigger -Weekly -At "12/16/2022 11pm" -DaysOfWeek Friday
+$Settings = New-ScheduledTaskSettingsSet -Hidden -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -WakeToRun -ExecutionTimeLimit 0 -Verbose
+New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings -Verbose
+Register-ScheduledTask -TaskName 'Reboot' -Action $Action -Settings $settings -Trigger $Trigger -RunLevel Highest -User system
