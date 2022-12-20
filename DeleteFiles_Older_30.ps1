0943495211f93a1469c347b68bd3a0b0709a5dbc
erez669 <57612608+erez669@users.noreply.github.com>
2022-12-20 19:47:17 +0200

    Delete Files Older than 30 days
    Delete Files Older than 30 days

---
+++ DeleteFiles_Older_30.ps1
@@ -0,0 +1,20 @@
+﻿If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+
+<# you must enable LongPath DWORD in registry on path
+
+"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem" in order to use this script #>
+
+clear
+
+Set-Location d:\
+$Daysback = "-30"
+$CurrentDate = Get-Date
+$excluded = @("killtask.jpg , LogViewer.lnk")
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem * -Recurse -Exclude $excluded | Where-Object { "$_.LastWriteTime -lt $DatetoDelete" } | Remove-Item -Recurse -Verbose -Force -Exclude $excluded -ErrorAction SilentlyContinue
