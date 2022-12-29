474592b7966dc2bbd2c71ba8a71b8064a52e5e85
erez669 <57612608+erez669@users.noreply.github.com>
2022-12-29 13:50:56 +0200

    Update MoveOU_MultipleOjbejcts.ps1

--- MoveOU_MultipleOjbejcts.ps1
+++ MoveOU_MultipleOjbejcts.ps1
@@ -1,6 +1,7 @@
-﻿$computerlist = get-content -path "\\10.251.10.251\supergsrv\HdMatte\PowerShell\Computers.txt"
+Import-Module activedirectory
+$computerlist = get-content -path "\\10.251.10.251\supergsrv\HdMatte\PowerShell\Computers.txt"
 foreach ($computer in $computerlist) {
 $newou = "OU=Test,OU=MarlogRishon,OU=Workstations,DC=corp,DC=supersol,DC=co,DC=il"
-Get-ADComputer $computer |Move-ADObject -TargetPath $newou -Verbose # check the current AD object OU and moving it to the requested OU
+Get-ADComputer $computer | Move-ADObject -TargetPath $newou -Verbose # check the current AD object OU and moving it to the requested OU
 }
 Write-Host "operation completed successfully"
