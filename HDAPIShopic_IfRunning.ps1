2b26ac1a98b1552639862fc72c450787bd8bc0fc
erez669 <57612608+erez669@users.noreply.github.com>
2023-01-04 08:07:23 +0200

    Check if Service Running
    Check if Service Running

---
+++ HDAPIShopic_IfRunning.ps1
@@ -0,0 +1,28 @@
+﻿<# If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+
+$serviceName = 'HDAPIShopic'
+$service = Get-Service -Name $serviceName | Where-Object {$_.Status -eq "Stopped"}
+Start-Service -InputObject $service -Verbose
+Start-Sleep 07
+
+#>
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{  
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
+}
+
+$services = Get-Service | Where-Object {$_.Name -like "HD*" -and $_.Status -eq "Stopped"}
+
+foreach ($service in $services) {
+    Start-Service -InputObject $service -Verbose
+    Start-Sleep 06
+}
