207944f723dd4949516161cfda1920bf6ccaa8e7
erez669 <57612608+erez669@users.noreply.github.com>
2023-01-19 15:46:27 +0200

    Replace values in the file
    Replace values in the file

---
+++ ReplaceValue_OnFile.ps1
@@ -0,0 +1,6 @@
+﻿$name = $env:COMPUTERNAME.Substring(4,3)
+$path = 'c:\Program Files\Retalix\SCO.NET\App\sco.exe.config'
+$replacement = "posnlb$name"
+$text = Get-Content $path
+$text = $text -replace 'posnlb\d{3}',$replacement #this is a regular expression that find in our case 3 digits after the 'posnlb' value
+$text | Set-Content $path
