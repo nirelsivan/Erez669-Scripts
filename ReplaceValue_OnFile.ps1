05871b91dbe3e0527a9cbe33aa70da92ee9d0a0b
erez669 <57612608+erez669@users.noreply.github.com>
2023-01-20 20:33:45 +0200

    Update ReplaceValue_OnFile.ps1

--- ReplaceValue_OnFile.ps1
+++ ReplaceValue_OnFile.ps1
@@ -1,5 +1,6 @@
 $name = $env:COMPUTERNAME.Substring(4,3)
 $path = 'c:\Program Files (x86)\Retalix\SCO.NET\App\sco.exe.config'
+$value = 'posnlb'
 $replacement = "$value$name"
 $text = Get-Content $path
 $text = $text -replace '<URL>http://posnlb(.*?).posprod.supersol.co.il:4444/wsgpos/Service.asmx</URL>', "<URL>http://$replacement.posprod.supersol.co.il:4444/wsgpos/Service.asmx</URL>"
