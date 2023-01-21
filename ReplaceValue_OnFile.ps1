54454f6e30e0a9fbdb95ce7c4566ce7d429ad5ab
erez669 <57612608+erez669@users.noreply.github.com>
2023-01-21 22:09:42 +0200

    Update ReplaceValue_OnFile.ps1

--- ReplaceValue_OnFile.ps1
+++ ReplaceValue_OnFile.ps1
@@ -1,5 +1,6 @@
 $computername = $env:COMPUTERNAME
 $name = ($computername -split '-')[1]
+#on newer powershell version we will use the substring for computername split, for example $env:COMPUTERNAME.substring(4,3)
 $path = 'c:\Program Files (x86)\Retalix\SCO.NET\App\sco.exe.config'
 $value = 'posnlb'
 $replacement = "$value$name"
