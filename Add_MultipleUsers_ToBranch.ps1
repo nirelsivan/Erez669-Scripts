cfa402653e0bc625053d6280ea0ab2a2335446cd
erez669 <57612608+erez669@users.noreply.github.com>
2023-02-13 00:37:01 +0200

    Update Add_MultipleUsers_ToBranch.ps1

--- Add_MultipleUsers_ToBranch.ps1
+++ Add_MultipleUsers_ToBranch.ps1
@@ -1,14 +1,21 @@
-﻿clear
 Import-Module activedirectory
 
-$userList = Read-Host 'Enter a comma-separated list of user names (for example apple,banana,tomato)'
-$userArray = $userList.Split(',')
-$branchNumber = Read-Host 'Enter branch number'
-$groupName = "Branch_$branchNumber"
-$groupDN = "LDAP://CN=$groupName,OU=Branch Groups,OU=Group Objects,OU=OUs,DC=posprod,DC=supersol,DC=co,DC=il"
-$group = [ADSI]$groupDN
+while ($true) {
+    clear
+    $userList = Read-Host 'Enter a comma-separated list of user names (for example apple,banana,tomato)'
+    $userArray = $userList.Split(',')
+    $branchNumber = Read-Host 'Enter branch number'
+    $groupName = "Branch_$branchNumber"
+    $groupDN = "LDAP://CN=$groupName,OU=Branch Groups,OU=Group Objects,OU=OUs,DC=posprod,DC=supersol,DC=co,DC=il"
+    $group = [ADSI]$groupDN
 
-foreach ($user in $userArray) {
-    $userDN = (Get-ADUser -Identity $user).DistinguishedName
-    $group.Add("LDAP://$userDN")
+    foreach ($user in $userArray) {
+        $userDN = (Get-ADUser -Identity $user).DistinguishedName
+        $group.Add("LDAP://$userDN")
+        Start-Sleep -s 07
+    }
+
+    if ($userList -eq 'exit') {
+        break
+    }
 }
