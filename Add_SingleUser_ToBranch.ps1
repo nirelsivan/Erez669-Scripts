e50efb1ef6de72791e9f1b5823fc27173f48dc11
erez669 <57612608+erez669@users.noreply.github.com>
2023-02-13 00:32:40 +0200

    Update Add_SingleUser_ToBranch.ps1

--- Add_SingleUser_ToBranch.ps1
+++ Add_SingleUser_ToBranch.ps1
@@ -1,8 +1,15 @@
-﻿Import-Module activedirectory
-$user = Read-Host 'enter username to add branch group'
-$branchNumber = Read-Host 'enter branch number'
-$groupName = "Branch_$branchNumber"
-$groupDN = "LDAP://CN=$groupName,OU=Branch Groups,OU=Group Objects,OU=OUs,DC=posprod,DC=supersol,DC=co,DC=il"
-$group = [ADSI]$groupDN
-$userDN = (Get-ADUser -Identity $user).DistinguishedName
-$group.Add("LDAP://$userDN")
+Import-Module activedirectory
+
+while ($true) {
+    $user = Read-Host 'enter username to add branch group'
+    $branchNumber = Read-Host 'enter branch number'
+    $groupName = "Branch_$branchNumber"
+    $groupDN = "LDAP://CN=$groupName,OU=Branch Groups,OU=Group Objects,OU=OUs,DC=posprod,DC=supersol,DC=co,DC=il"
+    $group = [ADSI]$groupDN
+    $userDN = (Get-ADUser -Identity $user).DistinguishedName
+    $group.Add("LDAP://$userDN")
+    
+    if ($user -eq 'exit') {
+        break
+    }
+}
