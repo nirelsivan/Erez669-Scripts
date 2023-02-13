5d66a5f26ac99a9ed12c0ba104665165fa6623a7
erez669 <57612608+erez669@users.noreply.github.com>
2023-02-13 23:12:30 +0200

    Change web.config value from CSV
    Change web.config value from CSV

---
+++ ERN_Change_web.config.ps1
@@ -0,0 +1,13 @@
+﻿$Snif = $env:computername.Substring(8,3)
+$csvFile = "\\reportsrv01\pub\ERN.csv"
+$webConfigFile = "D:\retalix\fpstore\WSGpos\web.config"
+
+$Csv = Import-Csv $csvFile
+$filteredRows = $Csv | Where-Object { $_.SNIF.PadLeft(3,'0') -eq $Snif }
+$filteredRows | ForEach-Object {
+    $ern = $_.ERN
+    $content = Get-Content $webConfigFile
+    $updatedContent = $content | ForEach-Object { $_ -replace "100276", $ern }
+    Set-Content $webConfigFile $updatedContent
+    break
+}
