c824a50cfce3a18742d26d784a2ad17b4459f651
erez669 <57612608+erez669@users.noreply.github.com>
2023-02-27 11:06:11 +0200

    Replace Value on XML File
    Replace Value on XML File

---
+++ ReplaceValue_OnFile_w7_w10.ps1
@@ -0,0 +1,19 @@
+﻿$computername = $env:COMPUTERNAME
+$name = ($computername -split '-')[1]
+$paths = @('c:\retalix\wingpos\GroceryWinPos.exe.config', 'c:\program files\Retalix\SCO.NET\App\sco.exe.config', 'c:\Program Files (x86)\Retalix\SCO.NET\App\sco.exe.config')
+$value = 'posnlb'
+$replacement = "$value$name"
+
+foreach ($path in $paths) {
+    if (Test-Path $path) {
+        $text = [System.IO.File]::ReadAllText($path)
+
+        if ($text -match 'plpossrv') {
+            $text = $text -replace '<URL>http://plpossrv(.*?).posprod.supersol.co.il:4444/wsgpos/Service.asmx</URL>', "<URL>http://plpossrv$name.posprod.supersol.co.il:4444/wsgpos/Service.asmx</URL>"
+        } else {
+            $text = $text -replace '<URL>http://posnlb(.*?).posprod.supersol.co.il:4444/wsgpos/Service.asmx</URL>', "<URL>http://$replacement.posprod.supersol.co.il:4444/wsgpos/Service.asmx</URL>"
+        }
+
+        [System.IO.File]::WriteAllText($path, $text)
+    }
+}
